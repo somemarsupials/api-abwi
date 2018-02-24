a9d2fb46ce783e8dc3af362abd24be8303815c31
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-02-24 11:58:21 +0000

    Make smalls style change

--- controllers/projects/all.js
+++ controllers/projects/all.js
@@ -1,13 +1,16 @@
 const Project = require('../../database').models.project;
 
 module.exports = async function(req, res, next, model = Project) {
+  let data;
+
   try {
     let projects = await model.findAll();
-    let data = projects.map(async (project) => await project.json());
-    return Promise.all(data).then(data => res.json(data));
+    data = projects.map(async (project) => await project.json());
   } 
   catch (error) {
     return res.sendStatus(500);
   };
+
+  return Promise.all(data).then(data => res.json(data));
 };
 
