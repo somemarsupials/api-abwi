e48c1bfcf235b485052bfeb4a6d2cce9bebc5c5f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-02-24 12:57:13 +0000

    Add basic app.js file, with projects controller and middlewares

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// middlewares
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// routes
+
+app.use('/project', require('./controllers/projects/'));
+
+module.exports = app;
