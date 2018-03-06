66d94a882c0ac0f29262fb0c60ffdbfe990b6d23
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-06 13:14:26 +0000

    Add client and project seeders

---
+++ seeders/1-clients.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let now = new Date();
+let clients = [];
+
+for (let client = 0; client < 3; client++) {
+  clients.push({
+    id: client,
+    name: `client ${client}`,
+    createdAt: now,
+    updatedAt: now,
+  });
+};
+
+let names = clients.map(function(client) {
+  return client.name;
+});
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('clients', clients, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('clients', { name: names }, {});
+  },
+};
---
+++ seeders/2-projects.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let now = new Date();
+let projects = [];
+
+for (let project = 0; project < 5; project++) {
+  projects.push({
+    id: project,
+    title: `project ${project}`,
+    clientId: project % 3,
+    createdAt: now,
+    updatedAt: now,
+  });
+};
+
+let titles = projects.map(function(project) { 
+  return project.title;
+});
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('projects', projects, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('projects', { title: titles }, {});
+  },
+};
