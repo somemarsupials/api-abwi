2917743e081b84f08db16468f7861fbbfba606e4
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-03-06 17:15:20 +0000

    Add items route as a child of projects

--- controllers/projects/index.js
+++ controllers/projects/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// Project routes
+
 router.get('/', require('./all'));
 router.get('/:id', require('./show'));
 router.post('/', require('./create'));
 router.patch('/:id', require('./update'));
 router.delete('/:id', require('./delete'));
+
+// Client child routes
+
+router.use('/:projectId/items', require('./items/'));
 
 module.exports = router;
--- controllers/projects/items/index.js
+++ controllers/projects/items/index.js
@@ -4,6 +4,8 @@ const router = express.Router({ mergeParams: true });
 router.get('/', require('./all'));
 router.get('/:id', require('./show'));
 router.post('/', require('./create'));
+router.patch('/:id', require('./update'));
+router.delete('/:id', require('./delete'));
 
 module.exports = router;
 
